2aa9a52698d6a57d1fc771e8b60cb7efb8cbd18a
Crynowecer0 <crynowecer11@gmail.com>
2023-05-05 15:22:17 -0400

    progress on message model tests

--- test_message_model.py
+++ test_message_model.py
@@ -5,10 +5,11 @@
 #    python -m unittest test_message_model.py
 
 
+from app import app
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follow
+from models import db, User, Message, Follow, Like
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -19,7 +20,6 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
 
 # Now we can import app
 
-from app import app
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
@@ -54,7 +54,7 @@ class MessageModelTestCase(TestCase):
         self.assertEqual(len(u1.followers), 0)
 
     def test_create_message(self):
-        """TODO: write me"""
+        """test creating a message"""
 
         u1 = User.query.get(self.u1_id)
 
@@ -69,9 +69,8 @@ class MessageModelTestCase(TestCase):
 
         self.assertEqual(len(u1.messages), 1)
 
-
     def test_delete_message(self):
-        """TODO: write me"""
+        """test deleting a message"""
 
         u1 = User.query.get(self.u1_id)
 
@@ -93,12 +92,58 @@ class MessageModelTestCase(TestCase):
 
         self.assertIsNone(deleted_msg)
 
+    def test_like_a_message(self):
+        """test liking a message"""
+        u1 = User.query.get(self.u1_id)
 
+        self.assertEqual(len(u1.messages), 0)
 
+        message = Message(text="wahoo!")
+        u1.messages.append(message)
+        db.session.commit()
 
+        msg_in_db = Message.query.get(message.id)
 
+        self.assertEqual(msg_in_db.text, "wahoo!")
+        self.assertEqual(len(u1.messages), 1)
 
-# delete message
-# like message
-# unlike message
-# liked_by relationship
+        like = Like(user_id=u1.id, message_id=msg_in_db.id)
+
+        db.session.add(like)
+        db.session.commit()
+
+        liked_msg = Message.query.get(message.id)
+
+        self.assertEqual(len(liked_msg.liked_by), 1)
+
+    def test_unlike_a_message(self):
+        """test unliking a previously liked message"""
+        u1 = User.query.get(self.u1_id)
+
+        self.assertEqual(len(u1.messages), 0)
+
+        message = Message(text="wahoo!")
+        u1.messages.append(message)
+        db.session.commit()
+
+        msg_in_db = Message.query.get(message.id)
+
+        self.assertEqual(msg_in_db.text, "wahoo!")
+        self.assertEqual(len(u1.messages), 1)
+
+        like = Like(user_id=u1.id, message_id=msg_in_db.id)
+
+        db.session.add(like)
+        db.session.commit()
+
+        liked_msg = Message.query.get(message.id)
+
+        self.assertEqual(len(liked_msg.liked_by), 1)
+
+        db.session.delete(like)
+        db.session.commit()
+
+        self.assertEqual(len(liked_msg.liked_by), 0)
+
+    #TODO: should not be able to like a currently liked message
+    #TODO: should not be able to unlike message not currently liked
